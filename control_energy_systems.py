c5c6454fd7c20454eaa908f85e99caafe1764d3c
Ian Vliet <i.a.n.vanvliet@student.tudelft.nl>
2025-04-20 18:30:57 +0200

    Added first version for continues script that checks the power consumption of the p1 meter and devices and updates the state based on available power.

---
+++ control_energy_systems.py
@@ -0,0 +1,86 @@
+import asyncio
+from homewizard_energy import HomeWizardEnergyV1
+from utils import GracefulKiller, setup_logger, initialize_devices
+from contextlib import AsyncExitStack
+import logging
+import sys
+from device_classes import complete_device, socket_device, p1_device
+
+
+def get_total_available_power(all_devices: list[complete_device]):
+    return sum(device.get_instantaneous_power() for device in all_devices)
+
+
+def determine_socket_states(total_power, sorted_sockets: list[socket_device]):
+    # available power is defined as - total power
+    available_power = - total_power
+
+    for socket in sorted_sockets:
+        if socket.should_power_on(available_power):
+            socket.updated_state = True
+            available_power -= socket.max_power_usage
+        else:
+            socket.updated_state = False
+
+
+
+async def main_loop(all_devices: list[complete_device], sorted_sockets: list[socket_device], logger: logging.Logger):
+    try:
+        while True:
+            # TODO: Ensure that if one task fails (i.e. one device cannot do a measurement) 
+            # the code continues with the other devices and sends some kind of message about the failed measurement
+            async with asyncio.TaskGroup() as tg:
+                for device in all_devices:
+                    tg.create_task(device.perform_measurement(logger))
+            logger.info(f"===== devices info gathered =====")
+
+            total_power = get_total_available_power(all_devices)
+
+            determine_socket_states(total_power, sorted_sockets)
+            async with asyncio.TaskGroup() as tg:
+                for socket in sorted_sockets:
+                    tg.create_task(socket.update_power_state(logger))
+            logger.info(f"===== socket states updated =====")
+
+            await asyncio.sleep(30)
+    except asyncio.CancelledError:
+        logger.info("Loop cancelled gracefully...")
+    finally:
+        logger.info("Cleaning up before shutdown...")
+
+
+
+async def main(all_devices: list[complete_device]):
+    socket_devices = [device for device in all_devices if isinstance(device, socket_device)]
+    sorted_sockets = sorted(socket_devices, key=lambda d: d.priority)
+
+    logger = setup_logger(logging.INFO)
+    killer = GracefulKiller(logger)
+    async with AsyncExitStack() as stack:
+        hwe_devices = []
+        for device in all_devices:
+            device.hwe_device = await stack.enter_async_context(HomeWizardEnergyV1(host=device.ip_address))
+            hwe_devices.append(device.hwe_device)
+
+        main_task = asyncio.create_task(main_loop(all_devices, sorted_sockets, logger))
+
+        # wait for keyboardInterrupt to cancel the main task
+        await killer.wait_for_shutdown()
+
+        main_task.cancel()
+        try:
+            await main_task
+        except asyncio.CancelledError:
+            logger.warning("CancelledError propogated")
+        except KeyboardInterrupt:
+            logger.error("Unexpected KeyboardInterrupt caught during task cancellation!")
+
+        logger.info("Shutdown complete")
+        sys.exit(130)
+
+    #         # Get all data and remap v1 data to new v2 structure
+    #         # print(await socket_bike.combined())
+
+if  __name__ == "__main__":
+    all_devices = initialize_devices("config_devices.json")
+    asyncio.run(main(all_devices))
---
+++ device_classes.py
@@ -0,0 +1,142 @@
+import asyncio
+from homewizard_energy import HomeWizardEnergyV1
+from contextlib import AsyncExitStack
+import logging
+import sys
+
+
+class complete_device:
+    """
+    Class for all properties of the device
+    """
+    def __init__(self, ip_address, device_type, device_name, **kwargs):
+        self._ip_address = ip_address
+        self._device_type = device_type
+        self._device_name = device_name
+        self._hwe_device = None
+
+        # the (instantaneous) attributes that change due to each measurement
+        self.inst_power_usage = None
+        self.inst_current = None
+
+    @property
+    def ip_address(self):
+        return self._ip_address
+    
+    @property
+    def device_type(self):
+        return self._device_type
+    
+    @property
+    def device_name(self):
+        return self._device_name
+    
+    @property
+    def hwe_device(self):
+        return self._hwe_device
+
+    @hwe_device.setter
+    def hwe_device(self, device: HomeWizardEnergyV1):
+        self._hwe_device = device
+
+    
+    async def perform_measurement(self, logger: logging.Logger):
+        # Get device information, like firmware version
+        hwe_device_info = await self.hwe_device.device()
+        logger.info(hwe_device_info)
+
+        # Get measurement --> power and current
+        measurement = await self.hwe_device.measurement()
+        self.inst_power_usage = measurement.power_w
+        self.inst_current = measurement.current_a
+
+        # log power and current
+        logger.info(f"{self.device_name} power: {self.inst_power_usage}")
+        logger.info(f"{self.device_name} current: {self.inst_current}")
+
+
+    def get_instantaneous_power(self):
+        """
+        For a general device, we assume that this power can not be freed,
+        therefore we should just return power
+        """
+        return self.inst_power_usage
+    
+
+class socket_device(complete_device):
+    """
+    Class for homewizard energy socket
+    """
+    def __init__(self, ip_address: str, device_type, device_name: str, max_power_usage: int | float, priority: int, **kwargs):
+        super().__init__(ip_address, device_type, device_name, **kwargs)
+        self._max_power_usage = max_power_usage
+        self.priority = priority
+        # the (instantaneous) attributes that change due to each measurement  
+        self.inst_state = None
+        # whether the device should power on or off
+        self.updated_state = False
+
+
+    @property
+    def max_power_usage(self):
+        return self._max_power_usage
+    
+    async def perform_measurement(self, logger: logging.Logger):
+        # Get power and current measurement
+        measurement = await self.hwe_device.measurement()
+        self.inst_power_usage = measurement.power_w
+        self.inst_current = measurement.current_a
+
+        # get socket state
+        device_state = await self.hwe_device.state()
+        self.inst_state = device_state.power_on
+
+        # log the power, current and state
+        logger.info(f"{self.device_name} power: {self.inst_power_usage}")
+        logger.info(f"{self.device_name} current: {self.inst_current}")
+        logger.info(f"{self.device_name} power state: {self.inst_state}")
+
+    
+    def get_instantaneous_power(self):
+        """
+        For a socket a positive power indicates the power used by the socket, 
+        which can be made free by turning the socket off --> therefore this power should count to available power.
+        Since we define available power as negative power the function should return -power
+        """
+        return -self.inst_power_usage
+    
+
+    def should_power_on(self, available_power: int | float):
+        return self._max_power_usage <= available_power
+    
+
+    async def update_power_state(self, logger: logging.Logger):
+        await self.hwe_device.state(power_on=self.updated_state)
+        logger.info(f"{self.device_name} power state set to: {self.updated_state}")
+
+    
+
+class p1_device(complete_device):
+    """
+    Class for homewizard p1 meter
+    """
+    def __init__(self, ip_address, device_type, device_name, **kwargs):
+        super().__init__(ip_address, device_type, device_name, **kwargs)
+
+    async def perform_measurement(self, logger: logging.Logger):
+        # Get power and current measurement
+        measurement = await self.hwe_device.measurement()
+        self.inst_power_usage = measurement.power_w
+        self.inst_current = measurement.current_a
+
+        # log the power and current
+        logger.info(f"{self.device_name} power: {self.inst_power_usage}")
+        logger.info(f"{self.device_name} current: {self.inst_current}")
+
+    
+    def get_instantaneous_power(self):
+        """
+        For the P1 a negative power means that is the available power,
+        therefore we should just return power
+        """
+        return self.inst_power_usage
