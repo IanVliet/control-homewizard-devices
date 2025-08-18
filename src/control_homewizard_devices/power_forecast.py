8d00d40b0eeb52368b65d48e5b22af7fe18d9ff7
Ian Vliet <i.a.n.vanvliet@student.tudelft.nl>
2025-08-18 12:40:18 +0200

    Add second site of solar panels. Change config_devices to include the key "quartz_parameters" to only hold all parameters needed for a PVSite.

--- src/control_homewizard_devices/power_forecast.py
+++ src/control_homewizard_devices/power_forecast.py
@@ -2,24 +2,31 @@ from quartz_solar_forecast.forecast import run_forecast
 from quartz_solar_forecast.pydantic_models import PVSite
 from datetime import datetime
 import json
-from plotly import graph_objects as go
+import pandas as pd
+import plotly.express as px
+# from plotly import graph_objects as go
 
 if __name__ == "__main__":
     with open("config/config_devices.json", "r") as config_file:
         config_data = json.load(config_file)
     solar_panels_data = config_data["solar panels"]
+    list_of_predictions = []
+    today_date = datetime.today()
     for solar_panel_data in solar_panels_data:
-        site = PVSite(
-            latitude=solar_panel_data["latitude"],
-            longitude=solar_panel_data["longitude"],
-            capacity_kwp=solar_panel_data["total peak power"],
-            tilt=solar_panel_data["tilt"],
-            orientation=solar_panel_data["orientation"],
-        )
+        name = solar_panel_data["name"]
+        params = solar_panel_data["quartz_parameters"]
+        site = PVSite(**params)
 
-        predictions_df = run_forecast(site=site, ts=datetime.today(), nwp_source="icon")
-        predictions_df.to_csv("data/solar_prediction_20250817.csv")
-        predictions_df.to_parquet("data/solar_prediction_20250817.parquet")
-        fig = go.Figure(go.Scatter(y=predictions_df["power_kw"]))
-        fig.show()
-        break
+        df_forecast = run_forecast(site=site, ts=today_date, nwp_source="icon")
+        df_forecast.rename(columns={"power_kw": f"power_kw ({name})"}, inplace=True)
+        list_of_predictions.append(df_forecast)
+    df = pd.concat(list_of_predictions, axis=1)
+    df.to_csv("data/solar_prediction_20250817.csv")
+    df.to_parquet("data/solar_prediction_20250817.parquet")
+    fig = px.line(
+        df,
+        x=df.index,
+        y=df.columns,
+        title="Solar Power Forecast",
+    )
+    fig.show()
