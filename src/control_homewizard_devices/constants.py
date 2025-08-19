cb5353d776b0c2bcc42b4db15e15e9e6aea8f454
Ian Vliet <i.a.n.vanvliet@student.tudelft.nl>
2025-08-19 10:34:58 +0200

    remove constants used by scip and pulp

--- src/control_homewizard_devices/constants.py
+++ src/control_homewizard_devices/constants.py
@@ -1,6 +1,3 @@
-from enum import Enum
-
-
 DELTA_T_SEC = 60
 DELTA_T = DELTA_T_SEC / 3600  # Convert seconds to hours
 
@@ -9,38 +6,4 @@ MAX_GRID_DRAW = 1e6  # Maximum grid draw in watts
 ENERGY_STORED_PENALTY = 1e6
 EPSILON = 1e-3  # Small tolerance for numerical stability
 
-SCIP_BINARY = "B"
-SCIP_INTEGER = "I"
-SCIP_CONTINUOUS = "C"
-
 AGGREGATE_BATTERY = "aggregate_battery"
-
-
-class VariableCategory(Enum):
-    """
-    Enum for the different variable categories used in scheduling.
-    """
-
-    BINARY = "binary"
-    INTEGER = "integer"
-    CONTINUOUS = "continuous"
-
-    def to_pulp(self):
-        from pulp import (
-            LpBinary,
-            LpInteger,
-            LpContinuous,
-        )
-
-        return {
-            VariableCategory.BINARY: LpBinary,
-            VariableCategory.INTEGER: LpInteger,
-            VariableCategory.CONTINUOUS: LpContinuous,
-        }[self]
-
-    def to_scip(self):
-        return {
-            VariableCategory.BINARY: SCIP_BINARY,
-            VariableCategory.INTEGER: SCIP_INTEGER,
-            VariableCategory.CONTINUOUS: SCIP_CONTINUOUS,
-        }[self]
